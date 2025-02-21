1d703218128f50d9592f097bb374899aa0652089
matheusandrade <matheusandrade@outlook.de>
2025-02-21 16:51:20 +0000

    Primeiro commit

---
+++ app/src/main/java/com/example/appgorjeta/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.appgorjeta
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+// Valor total da conta
+    // Número de pessoas
+    // Porcentagem da gorjeta
+    // 10%, 15% ou 20%
+    // Calcular
+    // Limpar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        }
+    }
