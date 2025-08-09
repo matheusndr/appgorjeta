51dbe0ca40e0ccb5468eb30adfbe34685e48d70b
Matheus Andrade <matheusandrade@outlook.de>
2025-08-09 09:43:45 +0100

    Primeiro commit

--- app/src/main/java/com/example/appgorjeta/MainActivity.kt
+++ app/src/main/java/com/example/appgorjeta/MainActivity.kt
@@ -2,34 +2,11 @@ package com.example.appgorjeta
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.appgorjeta.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
-
-// Valor total da conta
-    // Número de pessoas
-    // Porcentagem da gorjeta
-    // 10%, 15% ou 20%
-    // Calcular
-    // Limpar
-
-    // Recuperar as views do layout
-    // ViewBinding
-
-    // Recuperar os radio buttons
-    // Calculo de tip
-    // Mostrar resultado
 
     private lateinit var binding: ActivityMainBinding
 
@@ -38,61 +15,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var percentage: Int = 0
-        binding.rbOptionOne.setOnCheckedChangeListener { _, ischecked ->
-            println("Roque1  Option one: $ischecked")
-            if (ischecked) {
-                percentage = 10
-            }
-        }
-        binding.rbOptionOne.setOnCheckedChangeListener { _, ischecked ->
-            println("Roque1  Option two: $ischecked")
-            if (ischecked) {
-                percentage = 15
-            }
-        }
-        binding.rbOptionOne.setOnCheckedChangeListener { _, ischecked ->
-            println("Roque1  Option three: $ischecked")
-            if (ischecked) {
-                percentage = 20
-            }
-        }
-        val adapter = ArrayAdapter.createFromResource(
-            this,
-            R.array.num_people,
-            android.R.layout.simple_spinner_item
-        )
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.spinnerNumberOfPeople.adapter = adapter
-var numOfPeopleSelected = 0
-        binding.spinnerNumberOfPeople.onItemSelectedListener =
-            object : AdapterView.OnItemSelectedListener {
-
-                override fun onItemSelected(
-                    parent: AdapterView<*>?,
-                    view: View?,
-                    position: Int,
-                    id:Long
-                ) {
-                    numOfPeopleSelected = position
-                }
-
-                override fun onNothingSelected(p0: AdapterView<*>?) {
-
-                }
-            }
-
         binding.btnDone.setOnClickListener {
             val totalTabletemp = binding.tieTotal.text
+            val numPeopleTemp = binding.tieNumPeople.text
+            val percentageTemp = binding.tiePercentage.text
 
-            if (totalTabletemp?.isEmpty() == true
+            if (totalTabletemp?.isEmpty() == true ||
+                numPeopleTemp?.isEmpty() == true ||
+                percentageTemp?.isEmpty() == true
             ) {
                 Snackbar
                     .make(binding.tieTotal, "Preencha todos os campos", Snackbar.LENGTH_LONG)
                     .show()
             } else {
                 val totalTable: Float = totalTabletemp.toString().toFloat()
-                val nPeople: Int = numOfPeopleSelected
+                val nPeople: Int = numPeopleTemp.toString().toInt()
+                val percentage: Int = percentageTemp.toString().toInt()
 
                 val totalTemp = totalTable / nPeople
                 val tips = totalTemp * percentage / 100
@@ -101,26 +39,23 @@ var numOfPeopleSelected = 0
                 val intent = Intent(this, SummaryActivity::class.java)
                 intent.apply {
                     putExtra("totalTable", totalTable)
-                    putExtra("numPeople", numOfPeopleSelected)
+                    putExtra("numPeople", nPeople)
                     putExtra("percentage", percentage)
                     putExtra("totalAmount", totalWithTips)
                 }
+                clean()
                 startActivity(intent)
-
-
-
-            }
-
-            binding.btnClean.setOnClickListener {
-
-                binding.tieTotal.setText("")
-                binding.rbOptionThree.isChecked = false
-                binding.rbOptionTwo.isChecked = false
-                binding.rbOptionOne.isChecked = false
-
-
             }
         }
 
+        binding.btnClean.setOnClickListener {
+            clean()
+        }
+    }
+
+    private fun clean() {
+        binding.tieTotal.setText("")
+        binding.tiePercentage.setText("")
+        binding.tieNumPeople.setText("")
     }
 }
--- app/src/main/java/com/example/appgorjeta/SummaryActivity.kt
+++ app/src/main/java/com/example/appgorjeta/SummaryActivity.kt
@@ -5,18 +5,34 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.appgorjeta.databinding.ActivitySummaryBinding
 
 class SummaryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySummaryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_summary)
+        binding = ActivitySummaryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Recupera os dados da Intent AQUI, logo no início
         val totalTable = intent.getFloatExtra("totalTable", 0.0f)
         val numPeople = intent.getFloatExtra("numPeople", 0f)
         val percentage = intent.getFloatExtra("percentage", 0f)
         val totalAmount = intent.getFloatExtra("totalAmount", 0.0f)
+
+        binding.tvPercentage.text = percentage.toString() + "%"
+        binding.tvTotalAmount.text = totalAmount.toString()
+        binding.tvTotalTable.text = totalAmount.toString()
+        binding.tvNumPeopleLabel.text = numPeople.toString()
+
+
+        binding.btnRefresh.setOnClickListener {
+            finish()
+        }
+
 
         println("Roquere1 $totalAmount")
 
